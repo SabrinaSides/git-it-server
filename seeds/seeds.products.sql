84510572391d963e548413d32d4652349e678cfd
Sabrina Sides <sabrinasides91@gmail.com>
2021-07-21 10:07:51 -0500

    added AWS S3 img urls to database

--- seeds/seeds.products.sql
+++ seeds/seeds.products.sql
@@ -1,8 +1,8 @@
-INSERT INTO products (productName, category, price, productInfo)
+INSERT INTO products (productName, category, img, price, productInfo)
     VALUES
-        ('Java', 'tshirts', 19.99, 'A great Java-themed shirt'),
-        ('Python', 'tshirts', 19.99, 'A great Python-themed shirt'),
-        ('JavaScript', 'tshirts', 19.99, 'A great JavaScript-themed shirt'),
-        ('React', 'tshirts', 19.99, 'A great React-themed shirt'),
-        ('Debug Mug', 'mugs', 9.99, 'Help debug with this relatable mug'),
-        ('Stack Overflow Mug', 'mugs', 9.99, 'Drink your coffee in a tribute to the real coding MVP');
+        ('Java', 'tshirts', 'https://getitcapstone.s3.us-east-2.amazonaws.com/images/java.jpg', 19.99, 'A great Java-themed shirt'),
+        ('Python', 'tshirts', 'https://getitcapstone.s3.us-east-2.amazonaws.com/images/python.jpg', 19.99, 'A great Python-themed shirt'),
+        ('JavaScript', 'tshirts', 'https://getitcapstone.s3.us-east-2.amazonaws.com/images/javascript.jpg', 19.99, 'A great JavaScript-themed shirt'),
+        ('React', 'tshirts', 'https://getitcapstone.s3.us-east-2.amazonaws.com/images/react.jpg', 19.99, 'A great React-themed shirt'),
+        ('Debug Mug', 'mugs', 'https://getitcapstone.s3.us-east-2.amazonaws.com/images/debug-mug.png', 9.99, 'Help debug with this relatable mug'),
+        ('Stack Overflow Mug', 'mugs', 'https://getitcapstone.s3.us-east-2.amazonaws.com/images/stackoverflow-mug.png', 9.99, 'Drink your coffee in a tribute to the real coding MVP');
